ec814292c4a3421719986d115a565b66d5b0c629
João V <73762324+JoaoV-A01@users.noreply.github.com>
2022-10-20 22:49:08 -0300

    Update index.js

--- src/paginas/cadastro/index.js
+++ src/paginas/cadastro/index.js
@@ -1,72 +1,99 @@
-import React,{useState}  from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Component } from "react";
+import { Text,View, Button, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
+import { TextInput } from "react-native";
 
-export default function App() {
 
-  const [nome] = useState ('')
-
-  return (
-    <View style={styles.container}>
-      <Text>CADASTRO</Text>
-      <Text>Nome:</Text>
-      <TextInput
-        style={styles.input}
-        value={nome}
-        autoCapitalize="none"
-      />
-      <Text>Nota 1:</Text>
-      <TextInput
-        style={styles.input}
-        value={nome}
-        autoCapitalize="none"
-      />
-      <Text>Nota 2:</Text>
-      <TextInput
-        style={styles.input}
-        value={nome}
-        autoCapitalize="none"
-      />
-      <Text>Nota 3:</Text>
-      <TextInput
-        style={styles.input}
-        value={nome}
-        autoCapitalize="none"
-      />
-      <Text>Média:</Text>
-      <TextInput
-        style={styles.input}
-        value={nome}
-        autoCapitalize="none"
-      />
-
-    <TouchableOpacity style = {styles.botao}> CADASTRAR </TouchableOpacity>
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  botao: {
-    width: '90%',
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 10,
-    color: 'cyan'
-  },
-  input: {
-    width: '90%',
-    height: 30,
-    borderRadius: 3,
-    marginTop: 10,
-    borderWidth: 2,
-  
+export default class Cadastro extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.header}>Cadastro</Text>
+            <ScrollView>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="Nome"
+                    maxLength={50}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="Nota 1"
+                    maxLength={5}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="Nota 2"
+                    maxLength={5}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="Nota 3"
+                    maxLength={5}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="Foto de Perfil"
+                    maxLength={100}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TouchableOpacity
+                        style={styles.saveButton}
+                    >
+                        <Text style={styles.saveButtonText}>Cadastrar</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+          </View>
+        </View>
+      );
+    }
   }
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 45,
+      backgroundColor: '#F5FCFF',
+    },
+    header: {
+      fontSize: 25,
+      textAlign: 'center',
+      margin: 10,
+      fontWeight: 'bold'
+    }, 
+    inputContainer: {
+        paddingTop: 15
+      },
+      textInput: {
+        borderColor: '#CCCCCC',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        height: 50,
+        fontSize: 25,
+        paddingLeft: 20,
+        paddingRight: 20
+      },
+      saveButton: {
+        borderWidth: 1,
+        borderColor: '#007BFF',
+        backgroundColor: '#007BFF',
+        padding: 15,
+        margin: 5
+      },
+      saveButtonText: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center'
+      }
+  });
